11583ef0f58407707cc82cd71f3d4ec0aab987ab
Mohammad Sharik <boosaiyan@gmail.com>
2023-11-26 20:36:37 +0530

    s3 bucket create & destroy (#13)

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.26.0"
+    }
   }
 }
 
@@ -14,12 +18,13 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+  lower =true
+  upper =false
 }
 
+# resource "aws_s3_bucket" "example" {
+#    bucket = random_string.bucket_name.result
+#  }
 output "random_bucket_name" {
-         value = random_string.bucket_name.id
-}
-
-output "random_bucket_name_result" {
          value = random_string.bucket_name.result
 }
